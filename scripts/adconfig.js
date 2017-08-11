84dbfbf5da5bd3e390095937d3eab5bfb504cf82
Mohammad Manzoor Alam <mohammad.manzoor8972@gmail.com>
2017-08-11 16:54:10 +0100

    Demo Gap
    Demo

---
+++ scripts/adconfig.js
@@ -0,0 +1,7 @@
+var appName = "Capita Engage Demo";
+var appId = "328";
+var isPortalEnable=true;
+var isTranslationEnable=false;
+var isGoogleAdvertise=false;
+var isGoogleAnalytics=undefined;
+var buildEnvironment=2;
